b0d3697ebaac8a023c51ff418dc1f544fcf9f67e
sasageyo <84586484+heysm@users.noreply.github.com>
2022-10-08 16:03:21 +0530

    Create Navbar.jsx (#24)

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <h1>
+                Home
+            </h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Create</a>
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
